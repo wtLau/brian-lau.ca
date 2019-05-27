1482cec7c323b427ad03ce018809c1898f2e551f
Brian <lauwangtatbrian@gmail.com>
2019-05-27 20:48:38 +0800

    refactor tic-tac-toe into different folders

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
         "semi": ["error", "never"],
         "react/jsx-boolean-value": 0,
         "react/jsx-closing-bracket-location": 1,
-        "react/jsx-curly-spacing": [2, "always"],
+        "react/jsx-curly-spacing": [2, {"when": "never", "allowMultiline": false}],
         "react/jsx-indent-props": [1, 4],
         "react/jsx-no-undef": 1,
         "react/jsx-uses-react": 1,
