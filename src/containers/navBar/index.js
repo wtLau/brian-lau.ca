28a857a52e1fffee05db8d556a37597e9fc7b579
Brian <lauwangtatbrian@gmail.com>
2019-05-27 20:33:14 +0800

    React tic-tac-toe tutorial

---
+++ src/containers/navBar/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class index extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="square">
+                    {this.props.value}
+                </button>
+            </div>
+        )
+    }
+}
