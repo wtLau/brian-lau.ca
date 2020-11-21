ad805ca96a3bf96d5da1062565d20aa81a29503a
Brian <lauwangtatbrian@gmail.com>
2020-11-21 13:57:36 -0800

    manage package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   settings: {
     react: {
-      version: '^16.8.2'
-    }
+      version: '^16.8.2',
+    },
   },
   globals: {
     window: true,
     define: true,
     require: true,
-    module: true
+    module: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parser: 'babel-eslint',
@@ -20,11 +20,11 @@ module.exports = {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
       jsx: true,
-      es6: true
+      es6: true,
     },
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['babel', 'react'],
+  plugins: ['babel', 'react', 'react-hooks'],
   rules: {
     // indent: ['error', 2],
     // 'linebreak-style': ['error', 'unix'],
@@ -32,7 +32,7 @@ module.exports = {
     semi: ['error', 'never'],
     'react/jsx-boolean-value': 0,
     'react/jsx-closing-bracket-location': 1,
-    'react/jsx-curly-spacing': [2, { when: 'never', allowMultiline: false }],
+    'react/jsx-curly-spacing': [2, { when: 'never', allowMultiline: true }],
     'react/jsx-indent-props': [1, 2],
     'react/jsx-no-undef': 1,
     'react/jsx-uses-react': 1,
@@ -40,6 +40,8 @@ module.exports = {
     'react/jsx-wrap-multilines': 1,
     'react/react-in-jsx-scope': 1,
     'react/prefer-es6-class': 1,
-    'react/jsx-no-bind': 1
-  }
+    'react/jsx-no-bind': 1,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 }
