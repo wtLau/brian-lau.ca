358ad4455e15b70617949beb76b49a2535285c1e
Brian Lau- PC <lauwangtatbrian@gmail.com>
2021-10-13 23:49:45 -0700

    feat: styling

--- components/tools/MacroCalculator/ToolCard.tsx
+++ components/tools/MacroCalculator/ToolCard.tsx
@@ -4,23 +4,32 @@ import { Typography, makeStyles, Theme } from '@material-ui/core'
 
 interface IPost {
   slug: string
+  title: string
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
   mainGrid: {
     marginTop: theme.spacing(6),
   },
+  text: {
+    textTransform: 'capitalize',
+  },
 }))
 
-const ToolCard = ({ slug }: IPost) => {
+const ToolCard = ({ title, slug, ...RestaurantOutlined }: IPost) => {
   const cn = useStyles()
 
   return (
     <div className={cn.mainGrid}>
       <Link href={`/tools/${slug}`}>
         <div>
-          <Typography variant='h5' component={'h4'} gutterBottom>
-            {slug}
+          <Typography
+            variant='h5'
+            component={'h4'}
+            gutterBottom
+            className={cn.text}
+          >
+            {title}
           </Typography>
         </div>
       </Link>
