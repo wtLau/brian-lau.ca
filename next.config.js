e539d17f27381d31bd653000a50d29efd16b6cd6
Brian Lau <blau@appannie.com>
2022-04-19 21:57:05 -0700

    bug: fix target nextjs error

--- next.config.js
+++ next.config.js
@@ -5,22 +5,11 @@ module.exports = {
       'lh3.googleusercontent.com', // Google Image
     ],
   },
-  //Nelify specification
-  target: 'serverless',
   webpack: (config, { dev, isServer }) => {
     if (isServer) {
       require('./scripts/generate-sitemap.js')
       // require('./scripts/generate-rss')
     }
-
-    // Replace React with Preact only in client production build
-    // if (!dev && !isServer) {
-    //   Object.assign(config.resolve.alias, {
-    //     react: 'preact/compat',
-    //     'react-dom/test-utils': 'preact/test-utils',
-    //     'react-dom': 'preact/compat',
-    //   })
-    // }
 
     return config
   },
