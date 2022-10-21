860f2bdb6e5249d62dc83be3f9104f307e5e0ff8
wtlau <wtlau.dev@gmail.com>
2022-10-21 13:56:17 -0400

    refact: clean up

--- components/common/Navbar/NavBar.tsx
+++ components/common/Navbar/NavBar.tsx
@@ -57,6 +57,7 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import Fade from '@mui/material/Fade'
 import {
   Button,
+  Container,
   Divider,
   Drawer,
   IconButton,
@@ -66,6 +67,7 @@ import {
   ListItemIcon,
   ListItemText,
   Slide,
+  useTheme,
 } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import ThemeButton from './ThemeButton'
@@ -75,32 +77,25 @@ const navTile = 'Brian Lau'
 const navItems = [
   { title: `Blog`, path: `/blog` },
   { title: 'About', path: '/about' },
-  {
-    title: `Github`,
-    path: `https://github.com/wtLau`,
-    external: true,
-  },
-  {
-    title: `LinkedIn`,
-    path: `https://www.linkedin.com/in/brian-lau/`,
-    external: true,
-  },
+  // {
+  //   title: `Github`,
+  //   path: `https://github.com/wtLau`,
+  //   external: true,
+  // },
+  // {
+  //   title: `LinkedIn`,
+  //   path: `https://www.linkedin.com/in/brian-lau/`,
+  //   external: true,
+  // },
 ]
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window
   children: React.ReactElement
 }
 
-function HideOnScroll({ children, window }: Props) {
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
+function HideOnScroll({ children }: Props) {
   const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
   })
 
   return (
@@ -110,12 +105,8 @@ function HideOnScroll({ children, window }: Props) {
   )
 }
 
-function ScrollTop({ children, window }: Props) {
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
+function ScrollTop({ children }: Props) {
   const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
     disableHysteresis: true,
     threshold: 100,
   })
@@ -145,14 +136,13 @@ function ScrollTop({ children, window }: Props) {
   )
 }
 
-export default function NavBar({ window }: Props) {
+const NavBar = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false)
+  const theme = useTheme()
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen)
   }
-  const container =
-    window !== undefined ? () => window().document.body : undefined
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
@@ -193,39 +183,49 @@ export default function NavBar({ window }: Props) {
   return (
     <>
       <HideOnScroll>
-        <AppBar color='primary'>
-          <Toolbar>
+        <AppBar
+          sx={{
+            backgroundImage: 'none',
+            backgroundColor: theme.palette.background.default,
+            py: 2,
+          }}
+        >
+          <Toolbar
+            sx={{
+              maxWidth: theme.breakpoints.values.lg,
+              alignSelf: 'center',
+              width: '100%',
+            }}
+          >
+            <Box sx={{ flexGrow: 1, display: { sm: 'flex' } }}>
+              <Typography
+                variant='h6'
+                noWrap
+                component='a'
+                href='/'
+                sx={{
+                  mr: 2,
+                  // fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                {navTile}
+              </Typography>
+            </Box>
             <IconButton
-              color='inherit'
               aria-label='open drawer'
-              edge='start'
+              edge='end'
               onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
+              sx={{ display: { sm: 'none' } }}
             >
               <MenuIcon />
             </IconButton>
-            <Typography
-              variant='h6'
-              noWrap
-              component='a'
-              href='/'
-              // sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-              sx={{
-                mr: 2,
-                display: { xs: 'none', sm: 'flex' },
-                // fontFamily: 'monospace',
-                fontWeight: 700,
-                flexGrow: 1,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              {navTile}
-            </Typography>
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
               {navItems.map((item) => (
-                <Button key={item.title} sx={{ color: '#fff' }}>
+                <Button key={item.title} sx={{ color: 'inherit' }}>
                   {item.title}
                 </Button>
               ))}
@@ -238,7 +238,6 @@ export default function NavBar({ window }: Props) {
       </HideOnScroll>
       <Box component='nav'>
         <Drawer
-          container={container}
           variant='temporary'
           open={mobileOpen}
           onClose={handleDrawerToggle}
@@ -246,7 +245,7 @@ export default function NavBar({ window }: Props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
+            display: { xs: 'flex', sm: 'none' },
             '& .MuiDrawer-paper': {
               boxSizing: 'border-box',
               width: drawerWidth,
@@ -266,3 +265,4 @@ export default function NavBar({ window }: Props) {
     </>
   )
 }
+export default NavBar
