07ebf8f5a94057bfa2cb1a2671d6be5fac9c53a1
wtlau <wtlau.dev@gmail.com>
2022-11-10 12:27:58 -0800

    feat: add resources recommended feature

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,24 +1,69 @@
-import { Grid } from '@mui/material'
-import React, { useState, useEffect } from 'react'
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
 
 import PageLayout from '@components/layout/PageLayout'
-import { Link } from '@components/ui'
 import { resources, resourceType } from '@data/resources'
-import { skillsData, TSkills } from '@data/skillsData'
 import PostList from '@components/PostList'
 
 const Resources = () => {
+  const recomendList = resources.filter((list) => list.recommended === true)
+  const allList = resources.filter((list) => !list.recommended)
   return (
     <PageLayout
       title='Resources'
-      description='A little library of my knowledge collections.'
+      description='A little library of public knowledge.'
     >
-      <Grid item container direction='column'>
-        {resources.map((resource: resourceType) => (
-          <Grid item key={resource.title}>
-            <PostList title={resource.title} summary='' href={resource.url} />
+      <Grid container spacing={4}>
+        <Grid item container direction='column'>
+          <Grid item>
+            <Typography variant='h2' gutterBottom>
+              Recommended
+            </Typography>
           </Grid>
-        ))}
+
+          {!recomendList.length && (
+            <Grid item>
+              <Typography variant='body1'>
+                No recommended found. Please let me know if you are interested
+                to learn more about it.
+              </Typography>
+            </Grid>
+          )}
+          {recomendList.map((resource: resourceType) => (
+            <Grid item key={resource.title}>
+              <PostList
+                title={resource.title}
+                summary={resource.summary}
+                href={resource.url}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        <Grid item container direction='column'>
+          <Grid item>
+            <Typography variant='h2' gutterBottom>
+              All List
+            </Typography>
+          </Grid>
+
+          {!allList.length && (
+            <Grid item>
+              <Typography variant='body1'>
+                No posts found. Please let me know if you are interested to
+                learn more about it.
+              </Typography>
+            </Grid>
+          )}
+          {allList.map((resource: resourceType) => (
+            <Grid item key={resource.title}>
+              <PostList
+                title={resource.title}
+                summary={resource.summary}
+                href={resource.url}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     </PageLayout>
   )
