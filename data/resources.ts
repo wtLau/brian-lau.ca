1064cdac21fd64c1c3f428c5b19f4dbca98e83b1
wtlau <wtlau.dev@gmail.com>
2022-11-10 12:27:58 -0800

    feat: add resources recommended feature

--- data/resources.ts
+++ data/resources.ts
@@ -3,19 +3,32 @@ export type resourceType = {
   title: string
   url: string
   author: string | string[]
+  summary: string
   type: 'blog' | 'article' | 'book' | 'video' | 'tweet' | 'people'
   tags?:
     | 'front end development'
     | 'software development'
     | 'system designs'
     | 'productivity'
+    | 'software career'
   recommended?: boolean
 }
 
 export const resources: resourceType[] = [
   {
+    id: 8,
+    title: 'Not Only Code',
+    summary: 'Tech. Career. Leadership.',
+    author: 'Gregory',
+    type: 'blog',
+    tags: 'software career',
+    url: 'https://www.notonlycode.org/',
+    recommended: true,
+  },
+  {
     id: 7,
     title: 'Kent C. Dodds',
+    summary: 'One stop shop for everything you need to build JavaScript apps.',
     author: 'Kent C. Dodds',
     type: 'blog',
     tags: 'front end development',
@@ -25,24 +38,25 @@ export const resources: resourceType[] = [
   {
     id: 6,
     title: 'Lee Robinson – Developer, writer, creator.',
+    summary: '',
     author: 'Lee Robinson',
     type: 'blog',
     tags: 'front end development',
     url: 'https://leerob.io/',
-    recommended: true,
   },
   {
     id: 5,
     title: 'Overreacted — A blog by Dan Abramov',
+    summary: '',
     author: 'Dan Abramov',
     type: 'blog',
     tags: 'front end development',
     url: 'https://overreacted.io/',
-    recommended: true,
   },
   {
     id: 4,
     title: "Don't Call Yourself A Programmer, And Other Career Advice",
+    summary: '',
     author: 'Patrick McKenzie',
     type: 'article',
     url: 'https://www.kalzumeus.com/2011/10/28/dont-call-yourself-a-programmer/',
@@ -51,6 +65,7 @@ export const resources: resourceType[] = [
   {
     id: 3,
     title: 'How to apply SOLID principles in React applications',
+    summary: '',
     author: 'Tomas Gold',
     type: 'article',
     url: 'https://medium.com/@tomgold_48918/how-to-apply-solid-principles-in-react-applications-6c964091a982',
@@ -58,17 +73,18 @@ export const resources: resourceType[] = [
   {
     id: 2,
     title: 'The Pragmatic Programmer',
-    author: ['David Thomas', 'Andrew Hunt'],
-    type: 'book',
-    url: 'https://pragprog.com',
+    summary: 'The #1 newsletter for engineering managers and senior engineers',
+    author: 'Gergely Orosz',
+    type: 'blog',
+    url: 'https://blog.pragmaticengineer.com/',
     recommended: true,
   },
   {
     id: 1,
     title: 'Eloquent JavaScript',
+    summary: '',
     author: 'Marijin Haverbeke',
     type: 'book',
     url: 'https://eloquentjavascript.net/Eloquent_JavaScript.pdf',
-    recommended: true,
   },
 ]
