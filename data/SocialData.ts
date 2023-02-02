54eb060f9a8253a9226a364ca3490267063d0b90
Brian Lau <blau@appannie.com>
2023-02-02 13:06:00 -0800

    add SocialDatas

---
+++ data/SocialData.ts
@@ -0,0 +1,44 @@
+import { Url } from 'url'
+type SocialData = {
+  name: string
+  link: string
+  description: string
+}
+
+export const SocialData: SocialData[] = [
+  {
+    name: 'GitHub',
+    link: 'https://www.github.com/wtlau',
+    description: 'A place where my collection of codes live. (including mine)',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/brian-lau/',
+    description: 'A place for connecting with professional.',
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/brian_wtLau',
+    description: 'A place where I share my (unfilterd) thoughts.',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/wtlau.raw/',
+    description: 'Where I share my public photographies.',
+  },
+  {
+    name: 'ADPList',
+    link: 'https://adplist.org/mentors/brian-lau',
+    description: 'Book a session with me! Free and accessible mentorship.',
+  },
+  {
+    name: 'YouTube (coming soon)',
+    link: 'https://www.youtube.com/channel/UC2wP2VVjty2IaQYx8GYizeQ',
+    description: 'Where I share my talks and pour-over videos',
+  },
+  {
+    name: 'YouTube (coming soon)',
+    link: 'https://www.youtube.com/channel/UC2wP2VVjty2IaQYx8GYizeQ',
+    description: 'Where I share my talks and pour-over videos',
+  },
+]
